cefeec386c7e016fa92dfcf8c05991cc3149b503
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-06 20:19:13 +0900

    add convenient sql

---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(
+  id integer PRIMARY KEY,
+  name text
+);
+
+INSERT INTO
+  users (id, name)
+VALUES
+  (1, 'user 1'),
+  (2, 'user 2'),
+  (3, 'user 3'),
+  (4, 'user 4'),
+  (5, 'user 5');
