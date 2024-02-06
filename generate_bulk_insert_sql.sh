60da8d90cd64330828c968f2cf092b07c754a111
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-06 20:45:49 +0900

    add sh

---
+++ generate_bulk_insert_sql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+output_file="bulk_insert.sql"
+
+echo "INSERT INTO users (id, name) VALUES" > $output_file
+for ((i=1; i<=1000000; i++)); do
+  echo "  ($i, 'user $i')," >> $output_file
+done
+
+# 最後の行のカンマを削除
+sed -i '' '$s/,$//' $output_file
+
+echo ";" >> $output_file
